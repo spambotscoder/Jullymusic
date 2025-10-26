7dd0d6c72d8c07bb242d43745d25838361f3b8e5
𝐒𝐏𝐀𝐌 𝐁𝐎𝐓 𝐂𝐎𝐃𝐄𝐑 🚩 <light12035@gmail.com>
2025-10-26 15:42:30 +0530

    Update START_IMG environment variable default URL
    Signed-off-by: 𝐒𝐏𝐀𝐌 𝐁𝐎𝐓 𝐂𝐎𝐃𝐄𝐑 🚩 <light12035@gmail.com>

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class Config:
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
         self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
         self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/haagg2.png")
-        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/zvziwk.jpg")
+        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/7nzwqw.jpg")
         self.VIDEO_PLAY: bool = getenv("VIDEO_PLAY", True)
 
     def check(self):
